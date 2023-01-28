6afd21a7c858093cc91b036e85b059aaa0e1b978
unknown <iszlai.bernadett17@gmail.com>
2023-01-28 13:50:18 +0200

    adding instructions for challenge

--- src/expressionsStatements/PlayingCat.java
+++ src/expressionsStatements/PlayingCat.java
@@ -4,7 +4,7 @@ import java.nio.file.DirectoryStream;
 
 public class PlayingCat {
     /*The cats spend most of the day playing. In particular, they play if the temperature is between 25 and 35 (inclusive).
-   Unless it is summer, then the upper limit is 45 (inclusive) instead of 35.
+Unless it is summer, then the upper limit is 45 (inclusive) instead of 35.
 Write a method isCatPlaying that has 2 parameters. Method needs to return true if the cat is playing, otherwise return false
 1st parameter should be of type boolean and be named summer it represents if it is summer.
 2nd parameter represents the temperature and is of type int with the name temperature.
@@ -25,10 +25,11 @@ Do not add the main method to the solution code. */
         System.out.println(isCatPlaying(true, 25));
 
     }
-    public static boolean isCatPlaying (boolean summer, int temperature){
-        if (summer && (temperature >=25 && temperature <=45)){
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer && (temperature >= 25 && temperature <= 45)) {
             return true;
-        } else if (!summer && (temperature >=25 && temperature <=35)){
+        } else if (!summer && (temperature >= 25 && temperature <= 35)) {
             return true;
         } else return false;
     }
